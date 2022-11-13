7c87aee5d710a9faef4a681794ce1589aff31380
Shaymoun <enter312123@gmail.com>
2022-11-13 19:56:38 +0100

    Added complete task hanlder

--- src/components/Tasks/Task.js
+++ src/components/Tasks/Task.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import classes from './Task.module.css'
 
-
-
-
 const Task = props => {
 	const deleteHandler = () => {
 		props.setTodos(props.todos.filter(el => el.id !== props.todo.id))
 	}
-	
+	const isCompleteHandler = () => {
+		
+		props.setTodos(
+			props.todos.map(item => {
+				if (item.id === props.todo.id) {
+					return { ...item, isCompleted: !item.isCompleted }
+				}
+				return item
+			})
+		)
+	}
+
 	return (
-		<li className={classes.task}>
+		<li className={`${classes.task} ${props.isCompleted && classes.completed}`}>
 			{props.title}
 			<div className={classes.tools}>
-				<button><i className="fa-solid fa-check"></i></button>
-				<button><i className="fa-solid fa-pen-to-square"></i></button>
-				<button onClick={deleteHandler}><i className="fa-solid fa-xmark"></i></button>
+				<button onClick={isCompleteHandler}>
+					<i className='fa-solid fa-check'></i>
+				</button>
+				<button>
+					<i className='fa-solid fa-pen-to-square'></i>
+				</button>
+				<button onClick={deleteHandler}>
+					<i className='fa-solid fa-xmark'></i>
+				</button>
 			</div>
 		</li>
 	)
